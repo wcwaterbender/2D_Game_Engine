4b842ef89526e7f54dbd22f006e71a6f8ceb9dfb
Samuel Cornish <samcornish96@gmail.com>
2018-07-31 22:21:21 -0700

    Initiated Repo
    migrated files

---
+++ BirchEngine - textures/BirchEngine/Src/Game.cpp
@@ -0,0 +1,73 @@
+#include "Game.h"
+#include "TextureManager.h"
+#include "GameObject.h"
+
+GameObject* player;
+
+
+Game::Game()
+{}
+
+Game::~Game()
+{}
+
+void Game::init(const char* title, int width, int height, bool fullscreen)
+{
+	int flags = 0;
+	
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		}
+
+		isRunning = true;
+	}
+
+	player = new GameObject("assets/man.png", renderer,0,0);
+
+
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+	case SDL_QUIT :
+		isRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+
+void Game::update()
+{
+	player->Update();
+}
+
+void Game::render()
+{
+	SDL_RenderClear(renderer);
+	player->Render();
+	SDL_RenderPresent(renderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
